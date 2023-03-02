5064766b89faced090ea8bd11427b593b977c182
Ken Sears <kjshugo@gmail.com>
2023-03-02 13:04:39 +1000

    Update 3: Strated 'Getting Started with Javascript Programming' Completed Part 1 Completed Part 2 Completed Part 3 Completed Part 4

---
+++ AccessingDOM.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+document.getElementById("numericsection").innerHTML = 30;
+
+document.getElementById("textsection").innerHTML = "Name: Leto and Eli";
+
+var a;
+a=10;
+var b, c;
+b=c=20;
+
+x=a+30;
+
+console.log("Value of a = " +a);
+console.log("Value of b = " +b);
+console.log("Value of c = " +c);
+console.log("Value of x = " +x);
+
+y=b*c;
+console.log("Value of y = " +y);
+
+StudentName = "Brianna (Upper Camel Case)";
+studentName = "John (Lower or Regular Camel Case)";
+student_name = "David (Underscore)";
+
+console.log("The variable with Upper Camel Case is : " + StudentName);
+console.log("The variable with Lower or Regular Camel Case is : " + studentName);
+console.log("The variable with Underscore Case is : " + student_name);
+
+singleQuoteString = 'Declared within single quotes';
+backtickString = `Declared withing back ticks`;
+
+console.log('The singleQuoteString: %s', singleQuoteString);
+console.log(`The backtickString : ${backtickString}`);
+
---
+++ SeparateHTMLandJS.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+document.write("Hello World! This message is from the JavaScript file!");
+
+console.log("Hello Again! This message is from the javascript file and meant for the console!");
